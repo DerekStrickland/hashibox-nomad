f3c643a56d93a27041ec5b4833c07e7758128092
Derek Strickland <derek.strickland@gmail.com>
2022-06-28 10:11:55 -0400

    remove dead files

--- scripts/vault-init.sh
+++
@@ -1,6 +0,0 @@
-
-
-bolt command run "VAULT_ADDR=http://192.168.60.30:8200 vault operator init" --targets=192.168.60.10 --run-as root --stream > .secrets/vault-init.json
-
-bolt command run "curl -i -X POST -H ""Content-Type: application/json"" --data '{\"secret_shares\": 1,\"secret_threshold\": 1}' http://192.168.60.30:8200/v1/sys/init" --targets=192.168.60.10 --run-as root  --stream > .secrets/vault-init.json
-
